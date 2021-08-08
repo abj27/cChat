3647305a26e752ca1bf5d3ffc20bfdecabde0d8a
Alberto Espinal <alberto.espinal.c@gmail.com>
2021-08-08 08:27:38 -0400

    Adding Boilerplate

---
+++ cChat/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace cChat.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
